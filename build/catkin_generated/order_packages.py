6ebe4dbcdc59914cf4b0ed90e886bdcd713391ea
NewYdd <852732248@qq.com>
2017-03-04 22:33:01 +0800

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/dtrobot/car/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/dtrobot/DetectRobot/devel;/home/dtrobot/catkin_ws/devel;/home/dtrobot/detectrobot/devel;/opt/ros/indigo".split(';') if "/home/dtrobot/DetectRobot/devel;/home/dtrobot/catkin_ws/devel;/home/dtrobot/detectrobot/devel;/opt/ros/indigo" != "" else []
---
+++ build/smartcar/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dtrobot/car/src/smartcar
+# Build directory: /home/dtrobot/car/build/smartcar
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
